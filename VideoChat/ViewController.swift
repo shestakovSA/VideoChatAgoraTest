650691b1db22b4c5d97ff5ce0e5c7ccad529081b
shestakovSA <57584525+shestakovSA@users.noreply.github.com>
2020-09-07 09:36:08 +0500

    Initial Commit

---
+++ VideoChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoChat
+//
+//  Created by Сергей Шестаков on 07.09.2020.
+//  Copyright © 2020 Сергей Шестаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
